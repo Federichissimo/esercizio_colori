b56ec83b1870fb0291aa969ea63f94b860f12b80
Federichissimo <federico.folda@gmail.com>
2022-12-14 12:30:22 +0100

    Aggiunta colori a script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+document.querySelector("#green").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+
+document.querySelector("#red").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+
+document.querySelector("#blue").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+
+document.querySelector("#yellow").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+
+document.querySelector("#black").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+
+document.querySelector("#purple").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
